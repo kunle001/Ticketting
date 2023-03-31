0c3a39e73dc3c3feb1e716c29305512046dcc617
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-31 13:18:46 +0100

    Authetication service errors

---
+++ auth/src/errors/notAuthorizedError.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./custom-error";
+
+
+export class NotAUthorizedError extends CustomError {
+  statusCode = 401;
+  constructor() {
+    super('Not Authorized');
+
+    Object.setPrototypeOf(this, NotAUthorizedError.prototype)
+  }
+
+  serializeErrors() {
+    return [{
+      message: 'Not authorized'
+    }]
+  }
+
+
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { json } from "body-parser";
 import 'express-async-errors';
+import { ConnectOptions } from 'mongoose'
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import dotenv from 'dotenv'
 
 
 import { currentUserRouter } from './routes/current-user'
@@ -10,8 +14,14 @@ import { signupRouter } from './routes/signup'
 import { errorHandler } from './midllewares/error-handlers'
 import { NotFoundError } from './errors/not-found-error'
 
+dotenv.config({ path: './config.env' })
 const app = express();
+app.set('trust proxy', true)
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  // secure: true,
+}))
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,8 +35,25 @@ app.all('*', async () => {
 app.use(errorHandler)
 
 
+const start = async () => {
+  if (!process.env) {
+    throw new Error('no JWT_KEY')
+  }
+  try {
+    await mongoose.connect(process.env.DB!.replace('<password>', process.env.DB_PASSWORD!), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+
+    console.log('DB conected')
+  } catch (err) {
+    console.log(err)
+  };
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
 
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
