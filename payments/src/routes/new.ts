65f61da1ccd07be83e2833598cb4197143527524
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-04-28 22:11:11 +0100

    payment route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -3,6 +3,9 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payments';
+import { PyamentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 
 const router = express.Router();
@@ -25,15 +28,29 @@ router.post('/api/payments', requireAuth, [
 
   if (order.status === OrderStatus.Cancelled) throw new BadRequestError('this order is cancelled');
 
-  await stripe.charges.create({
+  const charge = await stripe.charges.create({
     currency: 'usd',
     source: token,
     amount: order.price * 100
   });
 
 
+  const payments = Payment.build({
+    orderId,
+    stripeId: charge.id
+  });
+  console.log(payments)
+  await payments.save()
+
+  await new PyamentCreatedPublisher(natsWrapper.client).publish({
+    id: payments.id,
+    orderId: payments.orderId,
+    stripeId: payments.stripeId
+  })
+
+
   res.status(201).send({
-    sucesss: true
+    id: payments.id
   })
 
 });
