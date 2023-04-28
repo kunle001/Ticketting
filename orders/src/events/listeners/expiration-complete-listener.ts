b6adee2f2c99c09ad3de81ec7763b095af77a806
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-04-28 22:09:00 +0100

    publish an event earlier if order isnt cancelled

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -10,9 +10,13 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
 
-    const order = await Order.findById(data.orderId).populate('ticket')
+    const order = await Order.findById(data.orderId).populate('ticket');
 
-    if (!order) throw new NotFoundError('order not found')
+    if (!order) throw new NotFoundError('order not found');
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack()
+    };
 
     order.set({
       status: OrderStatus.Cancelled
